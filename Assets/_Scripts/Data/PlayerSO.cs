cc56ff45a5c0f57d54e0931af70c50a9f1544c1d
wooyoung-1 <wooyoung7749@gmail.com>
2025-09-01 11:14:30 +0900

    [Fix] 플레이어_레이어

--- Assets/_Scripts/Data/PlayerSO.cs
+++ Assets/_Scripts/Data/PlayerSO.cs
@@ -54,4 +54,5 @@ public class PlayerSO : ScriptableObject
 {
     [field: SerializeField] public PlayerGroundData GroundData { get; private set; }
     [field: SerializeField] public PlayerAirData AirData { get; private set; }
+    [field: SerializeField] public PlayerAttackData AttakData { get; private set; }
 }
